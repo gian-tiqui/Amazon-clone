acdddf6030a24f6a85f72816efed13f15099af67
gian <michaelgiantiqui3@gmail.com>
2023-08-13 10:52:31 +0800

    meh

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Meow from "./components/meh";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { useEffect } from "react";
 
 export default function App() {
   
+  useEffect(() => {
+    
+  })
+
   return (
   <div>
     <Header />
     <Meow />
     <Footer />
-  
   </div>
   );
 }
