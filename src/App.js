41bdc5bbfd8f48dd52e322ae132f40ea77ba18f9
gian <michaelgiantiqui3@gmail.com>
2023-08-13 10:36:04 +0800

    w

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Meow from "./components/meh";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function App() {
-
-  return <div>
+  
+  return (
+  <div>
+    <Header />
     <Meow />
+    <Footer />
+  
   </div>
+  );
 }
